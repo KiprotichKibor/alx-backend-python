1553d2c76f38b7d79d2de2ae0f2a4b64bf0272da
KiproticKibor <kiborotich24@gmail.com>
2024-09-18 17:29:17 +0300

    fixing lint errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-"""mypy"""
+"""
+Mypy
+"""
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Matching annotations
+    Creates a zoomed-in version of a tuple by repeating each element.
+
+    Args:
+        lst (Tuple): The input tuple to be zoomed.
+        factor (int, optional): The zoom factor. Defaults to 2.
+
+    Returns:
+        List: A new list where each element from the input tuple is repeated
+              'factor' number of times.
+
+    Example:
+        >>> zoom_array((1, 2, 3), 2)
+        [1, 1, 2, 2, 3, 3]
     """
     zoomed_in: List = [
         item for item in lst
