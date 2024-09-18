36abe97b7cb9c4386a25d34263030d43791a1e3e
KiproticKibor <kiborotich24@gmail.com>
2024-09-18 14:00:35 +0300

    simple string concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" basic annotations, concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    takes two string arguments
+    returns a concatinated string
+    """
+    return str1 + str2
