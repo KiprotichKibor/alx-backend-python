9eac732c3aba2e142135b4c9aba5eaf357e34af9
KiproticKibor <kiborotich24@gmail.com>
2024-09-18 16:01:03 +0300

    sum of a list of float numbers

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" sum """
+
+
+def sum_list(input_list: [float]) -> float:
+    """
+    calculates the sum of a given list of float numbers
+    """
+    return sum(input_list)
