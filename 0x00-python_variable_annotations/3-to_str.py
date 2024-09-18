2c6764524d277c131e06110f2ec75d3c5c118efc
KiproticKibor <kiborotich24@gmail.com>
2024-09-18 15:08:19 +0300

    using the str() to convert float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" stringify """
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The float number to convert.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
